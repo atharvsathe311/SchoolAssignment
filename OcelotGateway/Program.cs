77f63ff14f8d4259c61ee84d2b10cea5c1c7996b
Atharv Sathe <atharvsathe0302@gmail.com>
2024-11-15 12:00:56 +0530

    Ocilet for Swagger Added

--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -7,24 +7,19 @@ IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("ocelot.json")
     .Build();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerForOcelot(configuration);
+
 builder.Services.AddOcelot(configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwaggerForOcelotUI(opt => {
+  opt.PathToSwaggerGenerator = "/swagger/docs";
 
+}).UseOcelot().Wait();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-await app.UseOcelot();
 app.UseHttpsRedirection();
 app.Run();
 
